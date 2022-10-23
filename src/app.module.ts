e37f23dbb4a0feecd896184e26fecf0204b526f6
HigorC <higor_oliveira@outlook.com>
2022-10-23 17:35:02 +0100

    feat: e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TripsModule } from './trip/trip.module';
 import { PersonModule } from './person/person.module';
-import { TripService } from './trip/trip.service';
 
 @Module({
   imports: [TripsModule, PersonModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

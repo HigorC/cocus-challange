82801b71f35bc57d184b30f1f5cb5521e1d298bc
HigorC <higor_oliveira@outlook.com>
2022-10-23 13:17:42 +0100

    feat: add dynamoose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dynamoose from 'dynamoose';
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
+  dynamoose.aws.ddb.local("http://localhost:4566");
+
+//   const ddb = new dynamoose.aws.ddb.DynamoDB({
+//     "accessKeyId": null,
+//     "secretAccessKey": "SECRET",
+//     "region": "us-east-1"
+// });
+
+// Set DynamoDB instance to the Dynamoose DDB instance
+// dynamoose.aws.ddb.set(ddb);
+
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
